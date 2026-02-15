16329f8871813002a8fb5cb493b6fe069d8c9ae6
DrJBouille <jeanbaptiste0605@gmail.com>
2026-02-15 19:12:36 +0100

    change incoming call modal

--- apps/webrtc-app-react/src/app/main/components/incoming-call.tsx
+++ apps/webrtc-app-react/src/app/main/components/incoming-call.tsx
@@ -1,5 +1,7 @@
 import SquareIconButton from '../../shared/button/square-icon-button';
 import { Phone, PhoneOff } from 'lucide-react';
+import TitleText from '../../shared/text/title-text';
+import NormalText from '../../shared/text/normal-text';
 
 interface IncomingCallProps {
   pickup: (receiver: string) => Promise<void>
@@ -10,13 +12,16 @@ interface IncomingCallProps {
 function IncomingCall({ pickup, hangup, caller }: IncomingCallProps) {
   return(
     <div className="w-screen h-screen fixed inset-0 bg-black/30 flex items-center justify-center">
-      <div className="bg-white p-4 rounded-lg shadow-lg min-w-[300px]">
-        <h2 className="text-lg font-bold">Popup</h2>
+      <div className="flex flex-col gap-8 items-center bg-white p-4 rounded-lg shadow-lg px-16 py-12">
+        <div className="flex flex-col ">
+          <TitleText text={caller}/>
+          <NormalText text="Incoming call..."/>
+        </div>
 
-        <SquareIconButton icon={Phone} onClick={() => pickup(caller)}/>
-        <SquareIconButton icon={PhoneOff} onClick={hangup}/>
-
-        <button onClick={close}>Close</button>
+        <div className="flex gap-8">
+          <SquareIconButton icon={Phone} onClick={() => pickup(caller)}/>
+          <SquareIconButton icon={PhoneOff} onClick={hangup}/>
+        </div>
       </div>
     </div>
   );
